04ec9dde90fd599e2016c1fa5b5136753220af59
vijay <VijayIyer>
2024-11-11 04:00:09 -0500

    Update app module to connect to mongodb

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: process.env.MONGODB_CONNECTION_STRING,
+      database: process.env.MONGODB_DATABASE,
+      entities: [
+        __dirname + '/**/*.entity{.ts,.js}',
+      ],
+      ssl: true,
+      useUnifiedTopology: true,
+      useNewUrlParser: true
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private configService: ConfigService) {}
+}
