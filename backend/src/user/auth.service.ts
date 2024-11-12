ea9eaaeebce54155362ad4215bba3cb574046650
vijay <VijayIyer>
2024-11-12 07:38:46 -0500

    Add an authservice which will be used for signup and signin

---
+++ backend/src/user/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Injectable()
+export class AuthService {
+    constructor() {
+        
+    }
+    signup(email: string, password: string) {
+
+    }
+    signin(email: string, password: string) {
+        
+    }
+}
