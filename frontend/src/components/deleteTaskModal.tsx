abf35dcc588e2464abd7d42416d00b32c0a068b4
vijay <VijayIyer>
2025-02-02 08:12:27 -0500

    DeleteTaskModal - initial

--- frontend/src/components/deleteTaskModal.tsx
+++ frontend/src/components/deleteTaskModal.tsx
@@ -1,8 +1,23 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogContentText } from "@mui/material"
+
 type DeleteTaskModalProps = {
+    id: number,
     isOpen: boolean,
     onClose: () => void
 }
 
-export default (deleteTaskModalProps: DeleteTaskModalProps) => {
-    return <></>
+export default ({id, isOpen, onClose}: DeleteTaskModalProps) => {
+    return (
+        <Dialog open={isOpen} onClose={onClose}>
+            <DialogContent>
+                <DialogContentText>
+                    Are you sure you want to delete the task?
+                </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onClose}>Cancel</Button>
+                <Button type="submit">Delete</Button>
+            </DialogActions>
+        </Dialog>
+    )
 }
