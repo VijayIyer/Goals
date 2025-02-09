1757d2bac02aa216696b82945536ad5c312904dd
vijay <VijayIyer>
2025-02-08 22:54:08 -0500

    Add cors middleware

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const { Sequelize } = require('sequelize');
 const tasksRouter = require("./tasksRouter");
 require('dotenv').config();
 const PORT = process.env.PORT || 3000;
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
+
 app.use("/tasks", tasksRouter);
 app.listen(PORT, () => {
     console.log(`listening on port - ${PORT}`);
