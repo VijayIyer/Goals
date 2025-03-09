5bcc17a940aa7eed363626b001059b08952b7382
vijay <VijayIyer>
2025-03-09 15:46:36 -0400

    Remove unncessary getCompletedTasks method - this can be done on frontend or after filtering on getAllTasks method

--- backend/tasksRouter.js
+++ backend/tasksRouter.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const controller = require("./tasksController.js");
 
-router.get("/completed", controller.getCompletedTasks);
 router.get("/", controller.get);
 router.get("/:id", controller.getById);
 router.delete("/:id", controller.delete);
