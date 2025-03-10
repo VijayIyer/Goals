be01eddf4ea00024b250ba9d5711ed72e7a17815
vijay <VijayIyer>
2025-03-10 09:40:50 -0400

    New express backend - created from existing with_express branch

---
+++ backend/TODOS.md
@@ -0,0 +1,21 @@
+[ ] Use best industry practices for sequelize, like a config file for using sequelize instance
+    [ ] It should be possible to switch DB without making changes in code!!!
+[ ] Convert to es6 syntax
+[ ] Add routers for different endpoints
+    [ ] Add auth router for user auth functionality to support multiple users
+[ ] Add services for communicating with backend, instead of doing DB communication in controller itself
+[ ] Update Task Model
+    [ ] Add more validations as required
+[ ] Points Model for summarizing and keeping track of user's points on completed tasks!
+    - [X] Start with creating an endpoint for getting filtered value of how many tasks are completed vs total
+[ ] Complete Project to Typescript
+
+[ ] Improve backend design for following capabilities
+- [ ] Add marking task completed ability 
+- [ ] Sub-tasks 
+- [ ] Daily / Repeated tasks 
+- [ ] Percentage completed 
+- [ ] Individual sub-tasks can hold a percentage of overall task 
+- [ ] Long running tasks
+
+[ ] Tasks controller - return error for date in the past
---
+++ backend/tasksController.js
@@ -0,0 +1,95 @@
+const { Op } = require("sequelize");
+const TaskModel = require("./taskModel.js");
+const controller = {
+  get: async (req, res) => {
+    try {
+      const { completed, viewingDate } = req.query;
+      console.log(
+        `req.query - ${JSON.stringify(req.query)}, ${completed === "true"}}`
+      );
+      if (completed === "true") {
+        return res.status(200).json(
+          await TaskModel.findAll({
+            where: {
+              [Op.and]: [
+                { completed: true },
+                {
+                  deadline: { [Op.startsWith]: viewingDate ? viewingDate : "" },
+                },
+              ],
+            },
+          })
+        );
+      }
+      return res.status(200).json(
+        await TaskModel.findAll({
+          where: {
+            deadline: { [Op.startsWith]: viewingDate ? viewingDate : "" },
+          },
+        })
+      );
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  },
+  getById: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const task = await TaskModel.findOne({ where: { id } });
+      if (!task)
+        return res.status(404).json({
+          message: `No task with id ${id} was found`,
+        });
+      return res.status(200).json(task);
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  },
+  delete: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const task = await TaskModel.destroy({ where: { id } });
+      return res.status(204).send();
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json(err);
+    }
+  },
+  update: async (req, res) => {
+    const { id } = req.params;
+    const { title, description, deferred, deadline, completed } = req.body;
+    try {
+      await TaskModel.update(
+        {
+          title,
+          description,
+          deferred,
+          deadline,
+          completed,
+        },
+        {
+          where: { id },
+          returning: true,
+          plain: true,
+        }
+      ).then(async (result) => {
+        const response = await TaskModel.findByPk(id);
+        return res.status(200).json(response);
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json(err);
+    }
+  },
+  create: async (req, res) => {
+    try {
+      const task = await TaskModel.create({ ...req.body });
+      return res.status(201).json(task);
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json(err);
+    }
+  },
+};
+
+module.exports = controller;
