09e0d04411e87ffeffaca67b34b812396f9ca82c
vijay <VijayIyer>
2025-07-05 18:22:06 -0400

    Move some util methods to date utils

--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -1,11 +1,31 @@
 import { DateRange } from '../types';
 import { getDateWeek } from './week';
-import { FilterBy } from '../enums';
+import { FilterBy, GroupBy } from '../enums';
 export function getRandomDateWithinRange(startDate: Date, endDate: Date): Date {
     const startTime = startDate.getTime();
     const endTime = endDate.getTime();
     const randomTime = startTime + Math.random() * (endTime - startTime);
     return new Date(randomTime);
+}
+
+export function getRangeForDate(deadline: Date, groupBy: GroupBy): string {
+    switch (groupBy) {
+        case GroupBy.DAY: {
+            return deadline.toLocaleDateString();
+        }
+        case GroupBy.WEEK: {
+            return getDateWeek(deadline).toString();
+        }
+        case GroupBy.MONTH: {
+            return deadline.toLocaleString('default', { month: 'long' });
+        }
+        case GroupBy.YEAR: {
+            return deadline.getFullYear().toString();
+        }
+        default: {
+            return deadline.toLocaleDateString();
+        }
+    }
 }
 
 export function getDefaultDateRange(filterBy: FilterBy | null): DateRange {
