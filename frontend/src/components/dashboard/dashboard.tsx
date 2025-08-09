d478b4be7482c0a6f9bccf96f183e82f20aca228
vijay <VijayIyer>
2025-08-09 17:30:39 -0400

    New object that fetches summary info from backend and displays in a card

--- frontend/src/components/dashboard/dashboard.tsx
+++ frontend/src/components/dashboard/dashboard.tsx
@@ -1,91 +1,19 @@
 import { useContext, useEffect, useState } from 'react';
 
 import { Button, CircularProgress, Grid2 as Grid } from '@mui/material';
-import { makeStyles } from '@mui/styles';
 
 import ServicesContext from '../../services/servicesProvider';
 import { TaskServiceClientFactory } from '../../services/taskServiceClientFactory';
 import { Task } from '../../types';
-import { formatWeekDateRange, formatDate } from '../../utils/date';
-import { FilterBy, GroupBy } from '../../enums';
-import { filterTasksByFilterBy, getGroupRange, groupTasksByGroupByValue } from '../../utils/tasks';
-import { getDateWeek, getWeekEndDate, getWeekStartDate } from '../../utils/week';
+import { createDateRangeObject, getCurrentYear } from '../../utils/date';
+import { FilterBy, GroupBy, RankStatType, StatType } from '../../enums';
+import { getDateWeek, getWeekStartDate } from '../../utils/week';
 
-import {
-    GroupedTasksType,
-    PerformanceInDateRange,
-    RequiredSummaryItemDetails as RequiredSummaryItemData,
-} from '../../interfaces';
+import { RequiredSummaryItemDetails as RequiredSummaryItemData } from '../../interfaces';
 
 import AddSummaryItemModal from './addSummaryItemModal';
 import SummaryItem from './summaryItem';
-
-const useStyles = makeStyles({
-    summaryItem: {
-        border: '1px solid',
-        padding: '1em',
-    },
-});
-
-function getPerformancesInDateRange(tasks: Array<Task>, filterBy: FilterBy, groupBy: GroupBy): PerformanceInDateRange {
-    const currentDateRangeTasks: Array<Task> = filterTasksByFilterBy(tasks, filterBy);
-    const groupedTasks: GroupedTasksType = groupTasksByGroupByValue(currentDateRangeTasks, groupBy);
-    const groupedSummaryInCurrentDateRange = Object.keys(groupedTasks).map(key => {
-        return {
-            name: getGroupRange(key, groupBy),
-            total: groupedTasks[key].length,
-            completed: groupedTasks[key].filter(task => task.completed === true).length,
-        };
-    });
-    const bestPerformingItem = groupedSummaryInCurrentDateRange.reduce(
-        (prev, current) => {
-            return !prev.name
-                ? current
-                : prev.completed / prev.total > current.completed / current.total
-                  ? prev
-                  : current;
-        },
-        { name: '', completed: 0, total: 1 },
-    );
-    const worstPerformingItem = groupedSummaryInCurrentDateRange.reduce(
-        (prev, current) => {
-            return !prev.name
-                ? current
-                : prev.completed / prev.total < current.completed / current.total
-                  ? prev
-                  : current;
-        },
-        { name: '', completed: 0, total: 1 },
-    );
-    return {
-        best: {
-            name: bestPerformingItem.name,
-            completed: bestPerformingItem.completed,
-            total: bestPerformingItem.total,
-            completionPercentage: bestPerformingItem.total
-                ? bestPerformingItem.completed / bestPerformingItem.total
-                : bestPerformingItem.completed / 1,
-        },
-        worst: {
-            name: worstPerformingItem.name,
-            completed: worstPerformingItem.completed,
-            total: worstPerformingItem.total,
-            completionPercentage: worstPerformingItem.total
-                ? worstPerformingItem.completed / worstPerformingItem.total
-                : worstPerformingItem.completed / 1,
-        },
-    };
-}
-
-function SummaryGridItem({ title, children }: { title: string; children: React.ReactNode }) {
-    const classes = useStyles();
-    return (
-        <Grid className={classes.summaryItem} columns={3}>
-            <h3 style={{ wordBreak: 'break-word' }}>{title}</h3>
-            {children}
-        </Grid>
-    );
-}
+import SummaryItemFromRequestedObject from './summaryItemFromRequestedObject';
 
 function Dashboard() {
     const { serviceType } = useContext(ServicesContext);
@@ -113,45 +41,6 @@ function Dashboard() {
     const handleAddSummaryItemSelection = (newSummaryItem: RequiredSummaryItemData) => {
         setSummaryItems([...summaryItems, newSummaryItem]);
     };
-
-    const overAllCompleted = tasks.filter(task => task.completed === true).length;
-    const overall = tasks.length;
-
-    const tasksInCurrentYear = tasks.filter(task => task.deadline.getFullYear() === new Date().getFullYear());
-    const tasksInCurrentMonth = tasks.filter(
-        task =>
-            task.deadline.toLocaleString('default', { month: 'long' }) ===
-                new Date().toLocaleString('default', { month: 'long' }) &&
-            task.deadline.getFullYear() === new Date().getFullYear(),
-    );
-    const tasksInCurrentWeek = tasks.filter(task => {
-        const currentDate = new Date();
-        const weekNumber = getDateWeek(currentDate);
-        const currentYear = new Date().getFullYear();
-        const taskDate = new Date(task.deadline);
-        const currentWeekStartDate = getWeekStartDate(weekNumber, currentYear);
-        const currentWeekEndDate = getWeekEndDate(weekNumber, currentYear);
-        taskDate.setHours(0, 0, 0, 0);
-        currentWeekStartDate.setHours(0, 0, 0, 0);
-        currentWeekEndDate.setHours(0, 0, 0, 0);
-        return taskDate >= currentWeekStartDate && (currentWeekEndDate ? taskDate <= currentWeekEndDate : true);
-    });
-    const todayTasks = tasks.filter(task => {
-        const currentDate = new Date();
-        const currentTaskDate = task.deadline;
-        currentTaskDate.setHours(0, 0, 0, 0);
-        currentDate.setHours(0, 0, 0, 0);
-        return currentTaskDate === currentDate;
-    });
-
-    const currentYearMonthPerformances = getPerformancesInDateRange(tasks, FilterBy.YEAR, GroupBy.MONTH);
-    const currentYearWeekPerformances = getPerformancesInDateRange(tasks, FilterBy.YEAR, GroupBy.WEEK);
-    const currentYearDayPerformances = getPerformancesInDateRange(tasks, FilterBy.YEAR, GroupBy.DAY);
-
-    const currentMonthWeekPerformances = getPerformancesInDateRange(tasks, FilterBy.MONTH, GroupBy.WEEK);
-    const currentMonthDayPerformances = getPerformancesInDateRange(tasks, FilterBy.MONTH, GroupBy.DAY);
-
-    const currentWeekDayPerformances = getPerformancesInDateRange(tasks, FilterBy.WEEK, GroupBy.DAY);
 
     if (isRefreshing) return <CircularProgress />;
 
@@ -182,113 +71,129 @@ function Dashboard() {
                 {summaryItems.map(summaryItem => (
                     <SummaryItem key={summaryItem.title} tasks={tasks} summaryItem={summaryItem} />
                 ))}
-                <SummaryGridItem title="Overall">
-                    <>
-                        <h4>Total: {overall}</h4>
-                        <h4>Completed: {overAllCompleted}</h4>
-                    </>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Current Year (${new Date().getFullYear()})`}>
-                    <>
-                        <h4>Total: {tasksInCurrentYear.length}</h4>
-                        <h4>Completed: {tasksInCurrentYear.filter(task => task.completed === true).length}</h4>
-                    </>
-                </SummaryGridItem>
-                <SummaryGridItem
-                    title={`Current Month (${new Date().toLocaleString('default', { month: 'long' })}, ${new Date().getFullYear()})`}
-                >
-                    <>
-                        <h4>Total: {tasksInCurrentMonth.length}</h4>
-                        <h4>Completed: {tasksInCurrentMonth.filter(task => task.completed === true).length}</h4>
-                    </>
-                </SummaryGridItem>
-
-                <SummaryGridItem
-                    title={`Current Week (${formatWeekDateRange(getDateWeek(new Date()), new Date().getFullYear())})`}
-                >
-                    <>
-                        <h4>Total: {tasksInCurrentWeek.length}</h4>
-                        <h4>Completed: {tasksInCurrentWeek.filter(task => task.completed === true).length}</h4>
-                    </>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Today (${formatDate(new Date())})`}>
-                    <>
-                        <h4>Total: {todayTasks.length}</h4>
-                        <h4>Completed: {todayTasks.filter(task => task.completed === true).length}</h4>
-                    </>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Best Month (${currentYearMonthPerformances.best.name})`}>
-                    <h4>
-                        Completion %: {(currentYearMonthPerformances.best.completionPercentage * 100).toFixed(2)} (
-                        {currentYearMonthPerformances.best.completed} / {currentYearMonthPerformances.best.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Worst Month (${currentYearMonthPerformances.worst.name})`}>
-                    <h4>
-                        Completion %: {(currentYearMonthPerformances.worst.completionPercentage * 100).toFixed(2)} (
-                        {currentYearMonthPerformances.worst.completed} / {currentYearMonthPerformances.worst.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Best Week (${currentYearWeekPerformances.best.name})`}>
-                    <h4>
-                        Completion %: {(currentYearWeekPerformances.best.completionPercentage * 100).toFixed(2)} (
-                        {currentYearWeekPerformances.best.completed} / {currentYearWeekPerformances.best.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Worst Week (${currentYearWeekPerformances.worst.name})`}>
-                    <h4>
-                        Completion %: {(currentYearWeekPerformances.worst.completionPercentage * 100).toFixed(2)} (
-                        {currentYearWeekPerformances.worst.completed} / {currentYearWeekPerformances.worst.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Best Day (${currentYearDayPerformances.best.name})`}>
-                    <h4>
-                        Completion %: {(currentYearDayPerformances.best.completionPercentage * 100).toFixed(2)} (
-                        {currentYearDayPerformances.best.completed} / {currentYearDayPerformances.best.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Worst Day (${currentYearDayPerformances.worst.name})`}>
-                    <h4>
-                        Completion %: {(currentYearDayPerformances.worst.completionPercentage * 100).toFixed(2)} (
-                        {currentYearDayPerformances.worst.completed} / {currentYearDayPerformances.worst.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Best Week (${currentMonthWeekPerformances.best.name})`}>
-                    <h4>
-                        Completion %: {(currentMonthWeekPerformances.best.completionPercentage * 100).toFixed(2)} (
-                        {currentMonthWeekPerformances.best.completed} / {currentMonthWeekPerformances.best.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Worst Week (${currentMonthWeekPerformances.worst.name})`}>
-                    <h4>
-                        Completion %: {(currentMonthWeekPerformances.worst.completionPercentage * 100).toFixed(2)} (
-                        {currentMonthWeekPerformances.worst.completed} / {currentMonthWeekPerformances.worst.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Best Day (${currentMonthDayPerformances.best.name})`}>
-                    <h4>
-                        Completion %: {(currentMonthDayPerformances.best.completionPercentage * 100).toFixed(2)} (
-                        {currentMonthDayPerformances.best.completed} / {currentMonthDayPerformances.best.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Worst Day (${currentMonthDayPerformances.worst.name})`}>
-                    <h4>
-                        Completion %: {(currentMonthDayPerformances.worst.completionPercentage * 100).toFixed(2)} (
-                        {currentMonthDayPerformances.worst.completed} / {currentMonthDayPerformances.worst.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Best Day (${currentWeekDayPerformances.best.name})`}>
-                    <h4>
-                        Completion %: {(currentWeekDayPerformances.best.completionPercentage * 100).toFixed(2)} (
-                        {currentWeekDayPerformances.best.completed} / {currentWeekDayPerformances.best.total})
-                    </h4>
-                </SummaryGridItem>
-                <SummaryGridItem title={`Worst Day (${currentWeekDayPerformances.worst.name})`}>
-                    <h4>
-                        Completion %: {(currentWeekDayPerformances.worst.completionPercentage * 100).toFixed(2)} (
-                        {currentWeekDayPerformances.worst.completed} / {currentWeekDayPerformances.worst.total})
-                    </h4>
-                </SummaryGridItem>
+                <SummaryItemFromRequestedObject title="Overall" statType={StatType.PERCENTAGE} />
+                <SummaryItemFromRequestedObject
+                    title="This Year"
+                    statType={StatType.PERCENTAGE}
+                    dateRange={createDateRangeObject(new Date(getCurrentYear(), 0, 1), null, FilterBy.YEAR)}
+                />
+                <SummaryItemFromRequestedObject
+                    title="This Month"
+                    statType={StatType.PERCENTAGE}
+                    dateRange={createDateRangeObject(
+                        new Date(getCurrentYear(), new Date().getMonth(), 1),
+                        new Date(getCurrentYear(), new Date().getMonth(), 1),
+                        FilterBy.MONTH,
+                    )}
+                />
+                <SummaryItemFromRequestedObject
+                    title="This Week"
+                    statType={StatType.PERCENTAGE}
+                    dateRange={createDateRangeObject(
+                        getWeekStartDate(getDateWeek(new Date()), getCurrentYear()),
+                        null,
+                        FilterBy.WEEK,
+                    )}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Best Month this year"
+                    statType={StatType.PERCENTAGE}
+                    rankStatType={RankStatType.BEST}
+                    rankStatGroupPeriod={GroupBy.MONTH}
+                    dateRange={createDateRangeObject(new Date(getCurrentYear(), 0, 1), null, FilterBy.YEAR)}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Worst Month this year"
+                    statType={StatType.PERCENTAGE}
+                    rankStatType={RankStatType.WORST}
+                    rankStatGroupPeriod={GroupBy.MONTH}
+                    dateRange={createDateRangeObject(new Date(getCurrentYear(), 0, 1), null, FilterBy.YEAR)}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Best Week this year"
+                    statType={StatType.PERCENTAGE}
+                    rankStatType={RankStatType.BEST}
+                    rankStatGroupPeriod={GroupBy.WEEK}
+                    dateRange={createDateRangeObject(new Date(getCurrentYear(), 0, 1), null, FilterBy.YEAR)}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Worst Week this year"
+                    statType={StatType.PERCENTAGE}
+                    rankStatType={RankStatType.WORST}
+                    rankStatGroupPeriod={GroupBy.WEEK}
+                    dateRange={createDateRangeObject(new Date(getCurrentYear(), 0, 1), null, FilterBy.YEAR)}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Best Week this month"
+                    statType={StatType.PERCENTAGE}
+                    rankStatType={RankStatType.BEST}
+                    rankStatGroupPeriod={GroupBy.WEEK}
+                    dateRange={createDateRangeObject(
+                        new Date(getCurrentYear(), new Date().getMonth(), 1),
+                        null,
+                        FilterBy.MONTH,
+                    )}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Worst Week this month"
+                    statType={StatType.PERCENTAGE}
+                    rankStatType={RankStatType.WORST}
+                    rankStatGroupPeriod={GroupBy.WEEK}
+                    dateRange={createDateRangeObject(
+                        new Date(getCurrentYear(), new Date().getMonth(), 1),
+                        null,
+                        FilterBy.MONTH,
+                    )}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Best day this week"
+                    statType={StatType.PERCENTAGE}
+                    rankStatType={RankStatType.BEST}
+                    rankStatGroupPeriod={GroupBy.DAY}
+                    dateRange={createDateRangeObject(
+                        new Date(getCurrentYear(), new Date().getMonth(), 1),
+                        null,
+                        FilterBy.WEEK,
+                    )}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Worst day this week"
+                    statType={StatType.COMPLETED}
+                    rankStatType={RankStatType.WORST}
+                    rankStatGroupPeriod={GroupBy.DAY}
+                    dateRange={createDateRangeObject(
+                        new Date(getCurrentYear(), new Date().getMonth(), 1),
+                        null,
+                        FilterBy.WEEK,
+                    )}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Best day this month"
+                    statType={StatType.PERCENTAGE}
+                    rankStatType={RankStatType.BEST}
+                    rankStatGroupPeriod={GroupBy.DAY}
+                    dateRange={createDateRangeObject(
+                        new Date(getCurrentYear(), new Date().getMonth(), 1),
+                        null,
+                        FilterBy.MONTH,
+                    )}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Worst day this month"
+                    statType={StatType.COMPLETED}
+                    rankStatType={RankStatType.WORST}
+                    rankStatGroupPeriod={GroupBy.DAY}
+                    dateRange={createDateRangeObject(
+                        new Date(getCurrentYear(), new Date().getMonth(), 1),
+                        null,
+                        FilterBy.MONTH,
+                    )}
+                />
+                <SummaryItemFromRequestedObject
+                    title="Today"
+                    statType={StatType.COMPLETED}
+                    dateRange={createDateRangeObject(new Date(), new Date(), FilterBy.DAY)}
+                />
                 {/* <Grid>
                     <div
                         style={{
---
+++ frontend/src/components/dashboard/summaryItemFromRequestedObject.tsx
@@ -0,0 +1,75 @@
+import { useContext, useEffect, useState } from 'react';
+import { Completed, DateRange, Percentage, RankStat } from '../../types';
+
+import { Grid2 as Grid } from '@mui/material';
+import { makeStyles } from '@mui/styles';
+
+import ServicesContext from '../../services/servicesProvider';
+import { TaskServiceClientFactory } from '../../services/taskServiceClientFactory';
+import { GroupBy, RankStatType, StatType } from '../../enums';
+import { getDateRangeString } from '../../utils';
+
+const useStyles = makeStyles({
+    summaryItem: {
+        border: '1px solid',
+        padding: '1em',
+    },
+});
+
+interface SummaryItemFromRequestedObjectProps {
+    title: string;
+    statType: StatType;
+    dateRange?: DateRange;
+    rankStatType?: RankStatType;
+    rankStatGroupPeriod?: GroupBy;
+}
+export default function SummaryItemFromRequestedObject({
+    title,
+    statType,
+    dateRange,
+    rankStatType,
+    rankStatGroupPeriod,
+}: SummaryItemFromRequestedObjectProps) {
+    const { serviceType } = useContext(ServicesContext);
+    const service = new TaskServiceClientFactory(serviceType).getServiceClient();
+    const [stat, setStat] = useState<Percentage | Completed | RankStat>();
+
+    useEffect(() => {
+        async function getCompletionStat() {
+            setStat(await service.getTaskCompletionInfo(statType, dateRange));
+        }
+
+        async function getRankStat() {
+            if (rankStatType && rankStatGroupPeriod)
+                setStat(await service.getRankStat(statType, rankStatType, rankStatGroupPeriod, dateRange));
+        }
+        if (rankStatType && rankStatGroupPeriod) getRankStat();
+        else getCompletionStat();
+    }, [statType]);
+    const classes = useStyles();
+    if (stat) {
+        if (rankStatType && rankStatGroupPeriod) {
+            return (
+                <Grid className={classes.summaryItem}>
+                    <h3 style={{ wordBreak: 'break-word' }}>{title}</h3>
+                    <h5>{(stat as RankStat).name}</h5>
+                    {statType === StatType.PERCENTAGE && (
+                        <h5>Percentage: {((stat as RankStat).value as Percentage).percentage.toFixed(2)}</h5>
+                    )}
+                    <h5>Completed: {(stat as RankStat).value.completed}</h5>
+                    <h5>Total: {(stat as RankStat).value.total}</h5>
+                </Grid>
+            );
+        }
+        return (
+            <Grid className={classes.summaryItem}>
+                <h3 style={{ wordBreak: 'break-word' }}>{title}</h3>
+                {dateRange && <h5>{getDateRangeString(dateRange)}</h5>}
+                {statType === StatType.PERCENTAGE && <h5>Percentage: {(stat as Percentage).percentage.toFixed(2)}</h5>}
+                <h5>Completed: {(stat as Completed).completed}</h5>
+                <h5>Total: {(stat as Completed).total}</h5>
+            </Grid>
+        );
+    }
+    return null;
+}
